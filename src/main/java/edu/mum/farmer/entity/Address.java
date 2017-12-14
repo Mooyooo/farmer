f58d3fc25305dbe45f4ca8b8500e7e39c13da564
Mooyooo <molomjamts@gmail.com>
2017-12-14 16:57:10 -0600

    The first commit

---
+++ src/main/java/edu/mum/farmer/entity/Address.java
@@ -0,0 +1,40 @@
+package edu.mum.farmer.entity;
+
+import javax.persistence.Embeddable;
+import javax.persistence.Entity;
+
+@Embeddable
+public class Address {
+	private String Street;
+	private String city;
+	private String state;
+
+	public String getStreet() {
+		return Street;
+	}
+
+	public void setStreet(String street) {
+		Street = street;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	public int getZipCode() {
+		return zipCode;
+	}
+	public void setZipCode(int zipCode) {
+		this.zipCode = zipCode;
+	}
+	private int zipCode;
+}
---
+++ src/main/java/edu/mum/farmer/entity/User.java
@@ -0,0 +1,88 @@
+package edu.mum.farmer.entity;
+
+import java.util.Date;
+
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+
+	@Id
+	@GeneratedValue
+	private long id;
+	private String firstname;
+	private String lastname;
+	private String username;
+	private String password;
+	private Date birthDate;
+	private String phone;
+	@Embedded
+	private Address address;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getFirstname() {
+		return firstname;
+	}
+
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+
+	public String getLastname() {
+		return lastname;
+	}
+
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Date getBirthDate() {
+		return birthDate;
+	}
+
+	public void setBirthDate(Date birthDate) {
+		this.birthDate = birthDate;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public Address getAddress() {
+		return address;
+	}
+
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+}
